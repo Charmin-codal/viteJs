3a6736f62fd7cf164db625e4f6c6b030f8131f0b
Charmin-codal <cpatel@codal.com>
2023-06-29 23:00:11 +0530

    Add a Project To Repo

---
+++ src/charmin/index.tsx
@@ -0,0 +1,10 @@
+import {} from "react";
+
+export default function Charmin (){
+     return (
+        <>
+        <h1>Hay How Are You</h1>
+        <p>My Name Is Charmin Patel</p>
+        </>
+     )
+}
